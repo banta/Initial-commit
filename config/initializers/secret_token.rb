f3fe9a3787ea1b906365d81b177739b3132f0a4e
Bernard Banta <banta.bernard@gmail.com>
2012-10-27 18:32:00 +0300

    Initial Commit
    This is the initial commit to nairuby

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+Nairuby::Application.config.secret_token = '3b25aa42f78203171dd116d0a680c7c4cc74e131e6fb9d6a5e4eeb4c9732f93f3c04277f4b0c1c1ccda7d8a27e1a6322645289c071d427d886b75990799f6a36'
